331d4c7bf42ecef72b4c1274895b3326cd4e227b
Renee Zha <renee.zha@u.northwestern.edu>
2017-04-08 13:31:14 -0500

    changed to follow Trump's account

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
 
-var stream = T.stream('statuses/filter', { follow: 268536594});
+var stream = T.stream('statuses/filter', { follow: 25073877});
 
 // var text = {
 //   status: "\"" + tweet.text + "\" #TrumpVerbatim"
