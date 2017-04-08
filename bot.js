902d14561c9d0df56847d64d8b8bb0b46d282142
Renee Zha <renee.zha@u.northwestern.edu>
2017-04-08 08:54:23 -0500

    Initial setup

---
+++ bot.js
@@ -0,0 +1,24 @@
+console.log("The bot is starting");
+
+var Twit = require('twit');
+
+var config = require('./config');
+var T = new Twit(config);
+
+// T.post('statuses/update', { status: 'hello world!' }, function(err, data, response) {
+//   console.log(data)
+// })
+
+var params = {
+  screen_name: "realDonaldTrump",
+  count: 5
+}
+
+T.get('statuses/user_timeline', params, gotData);
+
+function gotData (err, data, response) {
+  // var tweets = data.statuses;
+  // for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets);
+  // }
+}
---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key:         'nl6x83ddK9GUVUmVkBcv7Evgr',
+  consumer_secret:      'W9koELuanIpTOZmRQyT6QTZ4DIjNFa2MWmj5SN6Yofx71Uu4l0',
+  access_token:         '850660692357255168-ct3GHsQxJPjS2wPdQCySoqZnOvmRhsF',
+  access_token_secret:  'dfqR9UIRVX4xkBPP0GZbdkPel69boJwvvFRxEd5STvWP5',
+  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+}
