05e1b3abd074258acc0af14260715fdc45a68c95
Renee Zha <renee.zha@u.northwestern.edu>
2017-04-08 09:43:08 -0500

    Able to extract text from tweets

--- bot.js
+++ bot.js
@@ -9,16 +9,23 @@ var T = new Twit(config);
 //   console.log(data)
 // })
 
+// T.get('followers/ids', { screen_name: 'Renenenenee' },  function (err, data, response) {
+//   console.log(data)
+// })
+
 var params = {
-  screen_name: "realDonaldTrump",
-  count: 5
+  // screen_name: "realDonaldTrump",
+  // count: 1
+  q: 'from:realDonaldTrump',
+  count: 2
 }
 
-T.get('statuses/user_timeline', params, gotData);
+//T.get('statuses/user_timeline', params, gotData);
+T.get('search/tweets', params, gotData)
 
-function gotData (err, data, response) {
-  // var tweets = data.statuses;
-  // for (var i = 0; i < tweets.length; i++) {
-    console.log(data);
-  // }
+function gotData(err, data, response) {
+  var tweets = data.statuses;
+  for (var i = 0; i < tweets.length; i++) {
+  console.log(tweets[i].text);
+  }
 }
