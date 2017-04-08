e5b8d870348b7ce5bbd648a78f8b8dd5c26f1a1c
Renee Zha <renee.zha@u.northwestern.edu>
2017-04-08 08:58:03 -0500

    Modified bot.js

--- bot.js
+++ bot.js
@@ -19,6 +19,6 @@ T.get('statuses/user_timeline', params, gotData);
 function gotData (err, data, response) {
   // var tweets = data.statuses;
   // for (var i = 0; i < tweets.length; i++) {
-    console.log(tweets);
+    console.log(data);
   // }
 }
