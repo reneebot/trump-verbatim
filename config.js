6d1f60e4264ec863f3a48f0e062e8a526dc9e821
reneezha <reneezha@u.northwestern.edu>
2018-08-15 20:37:10 -0500

    add .gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  consumer_key:         'Kg1Q0MR8vtsvYvoEHhRn3evkZ',
-  consumer_secret:      'uVosCMjc82O2t3EcUPb8GZKyDLpE9tBwT5NSU9KdEfihSLadJG',
-  access_token:         '850612023209717760-XwacqvZ6zTzFkeKdIfGtiMyTf8lLaK4',
-  access_token_secret:  'RLEemTdVtWRxp0ct8QpSDh3poq3mVWTT9jv87Xwdhj4MI',
-  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-}
